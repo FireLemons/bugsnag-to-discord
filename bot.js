f43e32c6cdebd8bcd37e727b90b0581629b3d7ec
firelemons <shenyang64@gmail.com>
2023-09-02 09:55:41 -0500

    import config and create logger

--- bot.js
+++ bot.js
@@ -1,2 +1,24 @@
-console.log('test');
+var clc = require('cli-color');
+var fs = require('fs');
+var logger = {
+    info: function (message) {
+        console.log(clc.cyan(message));
+    },
+    error: function (message) {
+        console.log(clc.red(message));
+    },
+    warn: function (message) {
+        console.log(clc.yellow(message));
+    }
+};
+var https;
+try {
+    https = require('node:https');
+}
+catch (err) {
+    console.error(err);
+    throw new Error('https support is disabled');
+}
+var result = JSON.parse(fs.readFileSync('./config.json'));
+logger.info(result);
 //# sourceMappingURL=bot.js.map
--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,28 @@
-let https;
+const clc = require('cli-color')
+const fs = require('fs')
+
+const logger = {
+    info: (message: string) => {
+        console.log(clc.cyan(message))
+    },
+    error: (message: string) => {
+        console.log(clc.red(message))
+    },
+    warn: (message: string) => {
+        console.log(clc.yellow(message))
+    }
+
+}
+
+let https
 
 try {
-  https = require('node:https');
+    https = require('node:https')
 } catch (err) {
-  console.error('https support is disabled!');
-} 
+    console.error(err)
+    throw new Error('https support is disabled')
+}
+
+const result = JSON.parse(fs.readFileSync('./config.json'))
+
+logger.info(result)
