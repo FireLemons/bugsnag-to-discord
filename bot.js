70bafc671817e608e17c9f2521fe7be4d6328304
firelemons <shenyang64@gmail.com>
2023-09-12 20:02:37 -0500

    can fetch event details, created function to format event data into a discord message

--- bot.js
+++ bot.js
@@ -27,6 +27,7 @@ const https = __importStar(require("node:https"));
 const clc = require('cli-color');
 const CronJob = require('cron').CronJob;
 const fs = require('fs');
+const DISCORD_MESSAGE_LENGTH_LIMIT = 2000;
 const logger = {
     info: (message) => {
         console.log(clc.cyan(message));
@@ -39,18 +40,27 @@ const logger = {
     }
 };
 const config = JSON.parse(fs.readFileSync('./config.json'));
-function pollBugsnag() {
+function getBugsnagEventDetails(eventID) {
     return new Promise((resolve, reject) => {
+        if (typeof eventID !== 'string') {
+            logger.error('eventID must be a string');
+            reject(new TypeError('param eventID is not a string'));
+        }
+        if (!eventID.length) {
+            logger.error('Empty string eventID');
+            reject(new RangeError('param eventID is empty string'));
+        }
         let buffer = [];
         https.request({
             headers: {
                 Authorization: `token ${config.bugsnagAuthToken}`
             },
             hostname: 'api.bugsnag.com',
-            path: `/projects/${config.bugsnagProjectID}/events`
+            path: `/projects/${config.bugsnagProjectID}/events/${eventID}`
         }, (response) => {
             const { statusCode } = response;
             if (statusCode < 200 && 300 <= statusCode) {
+                logger.error(`Bugsnag event details data request returned with unsuccessful status: ${statusCode}`);
                 reject(response);
             }
             response.on('data', (chunk) => {
@@ -71,14 +81,77 @@ function pollBugsnag() {
         }).end();
     });
 }
-const pollBugsnagAndForwardToDiscord = new CronJob(`0/10 * * * *`, () => {
-    pollBugsnag().then((response) => {
-        logger.info('Response:');
-        console.log(response);
-    }).catch((error) => {
-        logger.error('Failure:');
-        console.error(error);
+function listBugsnagEvents() {
+    return new Promise((resolve, reject) => {
+        let buffer = [];
+        https.request({
+            headers: {
+                Authorization: `token ${config.bugsnagAuthToken}`
+            },
+            hostname: 'api.bugsnag.com',
+            path: `/projects/${config.bugsnagProjectID}/events`
+        }, (response) => {
+            const { statusCode } = response;
+            if (statusCode < 200 && 300 <= statusCode) {
+                logger.error(`Bugsnag event data request returned with unsuccessful status: ${statusCode}`);
+                reject(response);
+            }
+            response.on('data', (chunk) => {
+                buffer.push(chunk);
+            }).on('end', () => {
+                let bugsnagData;
+                try {
+                    bugsnagData = JSON.parse(Buffer.concat(buffer).toString());
+                }
+                catch (error) {
+                    reject(error);
+                    return;
+                }
+                resolve(bugsnagData);
+            });
+        }).on('error', (error) => {
+            reject(error);
+        }).end();
     });
-});
-pollBugsnagAndForwardToDiscord.start();
+}
+function formatDiscordMessage(bugsnagEvent) {
+    const anonymizedEmail = /(@.*)/.exec(bugsnagEvent.user.email)[0];
+    const matchedTimeSubstrings = /(.*):[\d]{2} GMT-[\d]{4} (\(.*\))/.exec(new Date(bugsnagEvent.received_at).toString());
+    const formattedTime = `${matchedTimeSubstrings[1]} ${matchedTimeSubstrings[2]}`;
+    const onlyException = bugsnagEvent.exceptions[0];
+    const relevantProjectFiles = onlyException.stacktrace.filter((subroutine) => {
+        return subroutine.in_project;
+    });
+    let stacktrace = '';
+    for (const projectFile of relevantProjectFiles) {
+        stacktrace = stacktrace +
+            `  **File:** ${projectFile.file}:${projectFile.line_number}
+\`\`\`ruby
+`;
+        for (const line of Object.values(projectFile.code)) {
+            stacktrace = stacktrace + line + '\n';
+        }
+        stacktrace = stacktrace + '```\n';
+    }
+    let message = `**Environment:** ${bugsnagEvent.app.releaseStage}
+**Message**: ${onlyException.message}
+**Error URL:** ${bugsnagEvent.url}
+**Stack:**
+${stacktrace}
+**Controller:** ${bugsnagEvent.context}
+**Affected User:** xxx${anonymizedEmail}, ${bugsnagEvent.user.id}
+**App URL:** ${bugsnagEvent.request.url}
+**Time:** ${formattedTime}
+`;
+    if (message.length > DISCORD_MESSAGE_LENGTH_LIMIT) {
+        while (message.length > DISCORD_MESSAGE_LENGTH_LIMIT - 4) {
+            message = message.substring(message.lastIndexOf("\n") + 1, -1);
+        }
+        message = message + '\n...';
+    }
+    return message;
+}
+const detailedTestingEvent = JSON.parse(fs.readFileSync('./sample_detailed_event.json'));
+logger.info('Discord Message:');
+console.log(formatDiscordMessage(detailedTestingEvent));
 //# sourceMappingURL=bot.js.map
--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,56 @@
-import { IncomingMessage } from "node:http"
 import * as https from "node:https"
 
 const clc = require('cli-color')
 const CronJob = require('cron').CronJob
 const fs = require('fs')
+
+type EventBugsnag = {
+    app: {
+        releaseStage: string
+    }
+    context: string
+    exceptions: [
+        {
+            errorClass: string
+            message: string
+            stacktrace: [
+                {
+                    file: string
+                    in_project: boolean
+                    line_number: number
+                    code: {
+                        [name: string] : string
+                    }
+                }
+            ]
+        }
+    ]
+    id: string
+    received_at: string
+    request: {
+        url: string
+    }
+    url: string
+    user: {
+        email: string
+        id: string
+    }
+}
+
+type EventBugsnagDetailed = EventBugsnag /* Type Intersection Operator -> */ & { // Used here to extend EventBugsnag into EventBugsnagDetailed
+    exceptions: [{
+        stacktrace: [{
+            file: string
+            in_project: boolean | null
+            line_number: number
+        }]
+    }]
+    user: {
+        email: string
+    }
+}
+
+const DISCORD_MESSAGE_LENGTH_LIMIT = 2000
 
 const logger = {
     info: (message: string) => {
@@ -20,8 +67,18 @@ const logger = {
 
 const config = JSON.parse(fs.readFileSync('./config.json'))
 
-function pollBugsnag (): Promise<Error | IncomingMessage | Object> {
+function getBugsnagEventDetails(eventID: string): Promise<EventBugsnagDetailed> {
     return new Promise((resolve, reject) => {
+        if (typeof eventID !== 'string') {
+            logger.error('eventID must be a string')
+            reject(new TypeError('param eventID is not a string'))
+        }
+
+        if (!eventID.length) {
+            logger.error('Empty string eventID')
+            reject(new RangeError('param eventID is empty string'))
+        }
+
         let buffer: Uint8Array[] = []
 
         https.request({
@@ -29,19 +86,20 @@ function pollBugsnag (): Promise<Error | IncomingMessage | Object> {
                 Authorization: `token ${config.bugsnagAuthToken}`
             },
             hostname: 'api.bugsnag.com',
-            path: `/projects/${config.bugsnagProjectID}/events`
+            path: `/projects/${config.bugsnagProjectID}/events/${eventID}`
         },
         (response) => {
             const { statusCode } = response
 
             if (statusCode < 200 && 300 <= statusCode) {
+                logger.error(`Bugsnag event details data request returned with unsuccessful status: ${statusCode}`)
                 reject(response)
             }
 
             response.on('data', (chunk) => {
                 buffer.push(chunk)
             }).on('end', () => {
-                let bugsnagData: Object
+                let bugsnagData: EventBugsnagDetailed
 
                 try {
                     bugsnagData = JSON.parse(Buffer.concat(buffer).toString())
@@ -58,17 +116,135 @@ function pollBugsnag (): Promise<Error | IncomingMessage | Object> {
     })
 }
 
-const pollBugsnagAndForwardToDiscord = new CronJob(
-    `0/10 * * * *`, // Every 10 minutes at 00, 10, 20...
-    () => {
-        pollBugsnag().then((response) => {
-            logger.info('Response:')
-            console.log(response)
-        }).catch((error) => {
-            logger.error('Failure:')
-            console.error(error)
-        })
-    }
-)
+function listBugsnagEvents(): Promise<EventBugsnag[]> {
+    return new Promise((resolve, reject) => {
+        let buffer: Uint8Array[] = []
 
-pollBugsnagAndForwardToDiscord.start()
+        https.request({
+            headers: {
+                Authorization: `token ${config.bugsnagAuthToken}`
+            },
+            hostname: 'api.bugsnag.com',
+            path: `/projects/${config.bugsnagProjectID}/events`
+        },
+        (response) => {
+            const { statusCode } = response
+
+            if (statusCode < 200 && 300 <= statusCode) {
+                logger.error(`Bugsnag event data request returned with unsuccessful status: ${statusCode}`)
+                reject(response)
+            }
+
+            response.on('data', (chunk) => {
+                buffer.push(chunk)
+            }).on('end', () => {
+                let bugsnagData: EventBugsnag[]
+
+                try {
+                    bugsnagData = JSON.parse(Buffer.concat(buffer).toString())
+                } catch (error) {
+                    reject(error)
+                    return
+                }
+
+                resolve(bugsnagData)
+            })
+        }).on('error', (error) => {
+            reject(error)
+        }).end()
+    })
+}
+
+function formatDiscordMessage (bugsnagEvent: EventBugsnagDetailed): string {
+    const anonymizedEmail = /(@.*)/.exec(bugsnagEvent.user.email)[0]
+    const matchedTimeSubstrings = /(.*):[\d]{2} GMT-[\d]{4} (\(.*\))/.exec(new Date(bugsnagEvent.received_at).toString())
+    const formattedTime = `${matchedTimeSubstrings[1]} ${matchedTimeSubstrings[2]}`
+
+    const onlyException = bugsnagEvent.exceptions[0]
+    const relevantProjectFiles = onlyException.stacktrace.filter((subroutine) => {
+        return subroutine.in_project
+    })
+
+    let stacktrace = ''
+
+    for (const projectFile of relevantProjectFiles) {
+        stacktrace = stacktrace +
+// Keep these template literals against the left. They preserve whitespace.
+`  **File:** ${projectFile.file}:${projectFile.line_number}
+\`\`\`ruby
+`
+        for (const line of Object.values(projectFile.code)) {
+            stacktrace = stacktrace + line + '\n'
+        }
+
+        stacktrace = stacktrace + '```\n'
+    }
+
+    let message =
+`**Environment:** ${bugsnagEvent.app.releaseStage}
+**Message**: ${onlyException.message}
+**Error URL:** ${bugsnagEvent.url}
+**Stack:**
+${stacktrace}
+**Controller:** ${bugsnagEvent.context}
+**Affected User:** xxx${anonymizedEmail}, ${bugsnagEvent.user.id}
+**App URL:** ${bugsnagEvent.request.url}
+**Time:** ${formattedTime}
+`
+
+    if (message.length > DISCORD_MESSAGE_LENGTH_LIMIT){
+        while (message.length > DISCORD_MESSAGE_LENGTH_LIMIT - 4) {
+            message = message.substring(message.lastIndexOf("\n") + 1, -1) 
+        }
+
+        message = message + '\n...'
+    }
+
+    return message
+}
+
+const detailedTestingEvent: EventBugsnagDetailed = JSON.parse(fs.readFileSync('./sample_detailed_event.json'))
+
+logger.info('Discord Message:')
+console.log(formatDiscordMessage(detailedTestingEvent))
+
+//const pollBugsnagAndForwardToDiscord = new CronJob(
+//    `0/30 * * * *`, // Every 30 minutes at XX:00 and XX:30
+//    () => {
+        // const currentTime = new Date()
+
+        // listBugsnagEvents().then((response) => {            
+        //     logger.info('Response:')
+
+        //     if (!(response instanceof Array)) {
+        //         logger.error(`Unexpected Bugsnag data response. Expected array got ${typeof response}`)
+        //         return
+        //     }
+
+        //     const errorEventsInLast30Minutes: EventBugsnag[] = response.slice(0, 1)/*filter((errorEvent: EventBugsnag) => {
+        //         return currentTime.valueOf() - new Date(errorEvent.received_at).valueOf() <= 1000 * 60 * 30
+        //     })*/
+
+        //     const eventCount: number = errorEventsInLast30Minutes.length
+
+        //     logger.info(`Found ${eventCount} events in the last 30 minutes`)
+
+        //     if (!eventCount) {
+        //         return
+        //     }
+
+        //     for(const bugsnagEvent of errorEventsInLast30Minutes) {
+        //         getBugsnagEventDetails(bugsnagEvent.id).then((bugsnagDetailedEvent: EventBugsnagDetailed) => {
+        //             console.log(JSON.stringify(bugsnagDetailedEvent))
+        //         })
+        //     }
+
+        //     console.log(errorEventsInLast30Minutes)
+        // }).catch((error) => {
+        //     logger.error('Failed to list Bugsnag events')
+        //     console.error(error)
+        // })
+//    }
+//)
+
+//pollBugsnagAndForwardToDiscord.start()
